cb2610ee6d2a99abd1edd04c40b15e42b5964b30
Irgey <116591589+Irgey@users.noreply.github.com>
2023-03-22 16:33:12 +0100

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+import { getRandomHexColor } from './random-color';
+const refs = {
+  startBtn: document.querySelector('[data-start'),
+  stopBtn: document.querySelector('[data-stop'),
+  body: document.querySelector('body'),
+};
+// let intervalId = null;
 
+refs.stopBtn.setAttribute('disabled', 'true');
+console.dir(refs.startBtn);
+refs.startBtn.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick() {
+  intervalId = setInterval(changeBodyColor, 1000);
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', 'true');
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+refs.stopBtn.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'true');
+}
